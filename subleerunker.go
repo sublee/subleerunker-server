990ea82d88baf9fb35f39f05772fe9d8527d4102
Heungsub Lee <sub@subl.ee>
2017-05-10 01:19:38 +0900

    Use Datastore instead of Memcache

--- subleerunker.go
+++ subleerunker.go
@@ -1,8 +1,9 @@
 // A simple HTTP server for world-best-score in SUBLEERUNKER.  It remembers
-// a score for about a month.
+// a score for about a week.
 package subleerunker
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -12,8 +13,25 @@ import (
 import (
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/memcache"
+	"google.golang.org/appengine/datastore"
 )
+
+type Score struct {
+	Score     int
+	ExpiresAt time.Time
+}
+
+type NotHigherScore struct {
+	Score     int
+	PrevScore int
+}
+
+func (n NotHigherScore) Error() string {
+	return fmt.Sprintf(
+		"score %d is not higher than prev score %d",
+		n.Score, n.PrevScore,
+	)
+}
 
 func init() {
 	fs := http.FileServer(http.Dir("."))
@@ -21,28 +39,33 @@ func init() {
 	http.HandleFunc("/score", score)
 }
 
-// Loads the current score from the Memcache.
-func loadScore(c context.Context) (int, error) {
-	item, err := memcache.Get(c, "score")
-	if err == memcache.ErrCacheMiss {
+func getKey(c context.Context) *datastore.Key {
+	return datastore.NewKey(c, "score", "score", 0, nil)
+}
+
+// Loads the current score from the Google Cloud Datastore.
+func loadScore(c context.Context, at time.Time) (int, error) {
+	key := getKey(c)
+	score := new(Score)
+	err := datastore.Get(c, key, score)
+	if err == datastore.ErrNoSuchEntity {
 		return 0, nil
-	} else if err == nil {
-		score, err := strconv.Atoi(string(item.Value))
-		if err != nil {
-			score = 0
-		}
-		return score, nil
-	} else {
+	} else if err != nil {
 		return -1, err
+	}
+	if at.After(score.ExpiresAt) {
+		return 0, nil
+	} else {
+		return score.Score, nil
 	}
 }
 
 // A handler for "GET /score".
 func getScore(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	score, err := loadScore(c)
+	score, err := loadScore(c, time.Now())
 	if err != nil {
-		http.Error(w, "Memcache Failed", http.StatusInternalServerError)
+		http.Error(w, "Datastore Failed", http.StatusInternalServerError)
 		return
 	}
 	fmt.Fprint(w, score)
@@ -50,32 +73,33 @@ func getScore(w http.ResponseWriter, r *http.Request) {
 
 // A handler for "PUT /score".
 func putScore(w http.ResponseWriter, r *http.Request) {
-	score, err := strconv.Atoi(r.FormValue("score"))
+	scoreValue, err := strconv.Atoi(r.FormValue("score"))
 	if err != nil {
 		http.Error(w, "Invalid Score Input", http.StatusBadRequest)
 		return
 	}
 	c := appengine.NewContext(r)
-	prevScore, err := loadScore(c)
+	err = datastore.RunInTransaction(c, func(c context.Context) error {
+		prevScoreValue, err := loadScore(c, time.Now())
+		if err != nil {
+			return err
+		}
+		if scoreValue < prevScoreValue {
+			return errors.New("not best score")
+		}
+		key := getKey(c)
+		score := &Score{
+			Score:     scoreValue,
+			ExpiresAt: time.Now().Add(7 * 24 * time.Hour), // after 7 days
+		}
+		_, err = datastore.Put(c, key, score)
+		return err
+	}, nil)
 	if err != nil {
-		http.Error(w, "Memcache Failed", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	if score < prevScore {
-		http.Error(w, "Not New Best Score", http.StatusBadRequest)
-		return
-	}
-	item := &memcache.Item{
-		Key:        "score",
-		Value:      []byte(strconv.Itoa(score)),
-		Expiration: 30 * 24 * time.Hour, // 30 days
-	}
-	err = memcache.Set(c, item)
-	if err != nil {
-		http.Error(w, "Memcache Failed", http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprint(w, score)
+	fmt.Fprint(w, scoreValue)
 }
 
 // A combined handler for every methods of "/score".
